a61508d93d98c120998420cf3080892acc20114c
Natalya Chernavina <natalya.ch@nimax.ru>
2016-05-31 00:36:55 +0600

    Задание 2 Модуль 1
    Дополнение к задаче

--- build/js/check.js
+++ build/js/check.js
@@ -2,26 +2,28 @@ function getMessage(a, b) {
   if (typeof a == 'boolean') {
     if (a) {
       return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
-    }
-    else {
+    } else {
       return 'Переданное GIF-изображение не анимировано';
     }
-  }
-  if (typeof a == 'number') {
-    return 'Переданное SVG-изображение содержит ' + a + ' объектов ' + (b * 4) + ' атрибутов';
-  }
-  if (typeof a == 'object' && typeof b != 'object') {
-    var sum = 0;
-    for (var i = 0; i < a.length; i++) {
-      sum += a[i];
+  } else {
+    if (typeof a == 'number') {
+      return 'Переданное SVG-изображение содержит ' + a + ' объектов ' + (b * 4) + ' атрибутов';
+    } else {
+      if (Array.isArray(a)) {
+        if (Array.isArray(b)) {
+          var square = 0;
+          for (var i = 0; i < a.length; i++) {
+            square += a[i] * b[i];
+          }
+          return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
+        } else {
+          var sum = 0;
+          for (var i = 0; i < a.length; i++) {
+            sum += a[i];
+          }
+          return 'Количество красных точек во всех строчках изображения: ' + sum;
+        }
+      }
     }
-    return 'Количество красных точек во всех строчках изображения: ' + sum;
-  }
-  if (typeof a == 'object' && typeof b == 'object') {
-    var square = 0;
-    for (var i = 0; i < a.length; i++) {
-      square += a[i] * b[i];
-    }
-    return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
   }
 }
