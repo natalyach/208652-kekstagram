271b1018bc30c2a0d0deba4d1baf28634c48ce60
Natalya Chernavina <natalya.ch@nimax.ru>
2016-06-27 02:07:35 +0600

    Модуль 5 Задание 1
    Выполнила задание + 2 дополнительных

--- src/pictures.js
+++ src/pictures.js
@@ -1,9 +1,10 @@
 'use strict';
-(function(window) {
+(function() {
   var picturesContainer = document.querySelector('.pictures');
   var filtersContainer = document.querySelector('.filters');
   var templateElement = document.querySelector('template');
   var elementToClone;
+  var pictures = [];
 
   filtersContainer.classList.add('hidden');
 
@@ -12,6 +13,11 @@
   } else {
     elementToClone = templateElement.querySelector('.picture');
   }
+
+  /** @constant {number} */
+  var XHR_LOAD_TIMEOUT = 10000;
+  /** @constant {string} */
+  var PICTURES_LOAD_URL = '//o0.github.io/assets/json/pictures.json';
 
   /**
    * @param {Object} data
@@ -37,9 +43,111 @@
     return element;
   };
 
-  window.pictures.forEach(function(picture) {
-    getPictureElement(picture, picturesContainer);
-  });
+  /** @param {function(Array.<Object>)} callback */
+  var getPictures = function(callback) {
+    var xhr = new XMLHttpRequest();
 
-  filtersContainer.classList.remove('hidden');
-})(window);
+    picturesContainer.classList.add('pictures-loading');
+
+    function errorHandler() {
+      picturesContainer.classList.remove('pictures-loading');
+      picturesContainer.classList.add('pictures-failure');
+    }
+
+    xhr.onerror = errorHandler;
+    xhr.ontimeout = errorHandler;
+    xhr.onload = function(evt) {
+      var request = evt.target;
+      picturesContainer.classList.remove('pictures-loading');
+      if(request.status === 200) {
+        var loadedData = JSON.parse(request.response);
+        callback(loadedData);
+      }
+    };
+
+    xhr.timeout = XHR_LOAD_TIMEOUT;
+    xhr.open('GET', PICTURES_LOAD_URL);
+    xhr.send();
+  };
+
+  /** @param {Array.<Object>} loadedPictures */
+  var renderPictures = function(loadedPictures) {
+    picturesContainer.innerHTML = '';
+    loadedPictures.forEach(function(picture) {
+      getPictureElement(picture, picturesContainer);
+    });
+  };
+
+  /**
+   * @param {Array.<Object>} loadedPictures
+   * @param {string} filter
+   */
+  var getFilteredPictures = function(loadedPictures, filter) {
+    var picturesToFilter = loadedPictures.slice(0);
+    switch (filter) {
+      case 'popular':
+        // Cписок фотографий, в том виде, в котором он был загружен
+        // Пока оставила кейс, вдруг дальше пригодится
+        break;
+      case 'new':
+        // Cписок фотографий, сделанных за последние четыре дня, отсортированные по убыванию даты
+        var today = new Date();
+        today.setHours(0, 0, 0, 0);
+        today.setDate(today.getDate() - 4);
+        var currentDate;
+        var filterPictures = picturesToFilter.filter(function(picture) {
+          currentDate = new Date(picture.date);
+          currentDate.setHours(0, 0, 0, 0);
+          return currentDate >= today;
+        });
+        filterPictures.sort(function(a, b) {
+          return new Date(b.date) - new Date(a.date);
+        });
+        picturesToFilter = filterPictures;
+        break;
+      case 'discussed':
+        // Cписок фотографий, отсортированные по убыванию количества комментариев
+        picturesToFilter.sort(function(a, b) {
+          return b.comments - a.comments;
+        });
+        break;
+    }
+    return picturesToFilter;
+  };
+
+  /** @param {string} filter */
+  var setFilterEnabled = function(filter) {
+    var filteredPictures = getFilteredPictures(pictures, filter);
+    if(filteredPictures.length <= 0) {
+      picturesContainer.classList.add('pictures-empty');
+    } else {
+      picturesContainer.classList.remove('pictures-empty');
+    }
+    renderPictures(filteredPictures);
+  };
+
+  var setFiltrationEnabled = function() {
+    var filters = filtersContainer.querySelectorAll('.filters-radio');
+    filters.forEach(function(filter) {
+      // Дополнительное задание раз
+      var filteredPictures = getFilteredPictures(pictures, filter.value);
+      var picturesCount = filteredPictures.length;
+      filter.nextElementSibling.innerHTML += '<sup>(' + picturesCount + ')</sup>';
+      // Дополнительное задание два
+      if(picturesCount <= 0) {
+        filter.setAttribute('disabled', true);
+        filter.nextElementSibling.style.opacity = 0.5;
+      }
+      filter.onchange = function(evt) {
+        setFilterEnabled(evt.target.value);
+      };
+    });
+    filtersContainer.classList.remove('hidden');
+  };
+
+  getPictures(function(loadedPictures) {
+    pictures = loadedPictures;
+    setFiltrationEnabled();
+    renderPictures(pictures);
+  });
+})();
